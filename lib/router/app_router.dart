b8760baed95099093954f8a13b1482a7c311a45f
Marc Peejay Viernes <viernes.marcpeejay@nesvier.com>
2024-10-12 19:09:22 +0800

    dev: poc for custome page transition

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -13,6 +13,26 @@ class AppRouter {
         builder: (BuildContext context, GoRouterState state) {
           return const HomePage();
         },
+        pageBuilder: (BuildContext context, GoRouterState state) {
+          return CustomTransitionPage(
+            key: state.pageKey,
+            child: const HomePage(),
+            transitionsBuilder: (
+              BuildContext context,
+              Animation<double> animation,
+              Animation<double> secondaryAnimation,
+              Widget child,
+            ) {
+              // Change the opacity of the page using a Curve based on the
+              // animation's value.
+              return FadeTransition(
+                opacity:
+                    CurveTween(curve: Curves.easeInOutCirc).animate(animation),
+                child: child,
+              );
+            },
+          );
+        },
       ),
     ],
   );
