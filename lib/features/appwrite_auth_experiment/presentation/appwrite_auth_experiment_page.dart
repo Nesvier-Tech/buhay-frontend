79b3be0cceabf6c9f9209fc8d4ba9dc235ee871c
Marc Peejay Viernes <viernes.marcpeejay@nesvier.com>
2024-11-06 22:35:46 +0800

    dev: update the google maps experiment using the newly added api keys

---
+++ lib/features/appwrite_auth_experiment/presentation/appwrite_auth_experiment_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppwriteAuthExperimentPage extends StatelessWidget {
+  const AppwriteAuthExperimentPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Appwrite Auth Experiment Page'),
+      ),
+    );
+  }
+}
