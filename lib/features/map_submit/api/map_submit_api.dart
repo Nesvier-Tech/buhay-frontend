dfc492f52f7ac072eba0544a03026846d497a41d
joshualfelipe <jlfelipe1@up.edu.ph>
2025-01-30 08:44:05 +0800

    fix: update the api request to post with parameters for directions

--- lib/features/map_submit/api/map_submit_api.dart
+++ lib/features/map_submit/api/map_submit_api.dart
@@ -5,10 +5,23 @@ import 'package:latlong2/latlong.dart';
 class MapSubmitApi {
   Future<Map<String, dynamic>> getRouteCoordinates(
       LatLng start, LatLng end) async {
-    final url =
-        'http://49.13.218.27:8080/directions?start=${start.longitude},${start.latitude}&end=${end.longitude},${end.latitude}';
+    // final url =
+    // 'http://49.13.218.27:8080/directions?start=${start.longitude},${start.latitude}&end=${end.longitude},${end.latitude}';
 
-    final response = await http.get(Uri.parse(url));
+    final url = 'http://10.0.2.2:8000/directions';
+
+    final body = json.encode({
+      'start': '${start.longitude},${start.latitude}',
+      'end': '${end.longitude},${end.latitude}'
+    });
+
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: body,
+    );
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
