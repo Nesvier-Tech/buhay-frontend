ec14b2d3fb34084897f3f7c3691fbaab852adc3d
joshualfelipe <jlfelipe1@up.edu.ph>
2025-03-04 09:35:24 +0800

    chore: comment import statement for single search page

--- lib/system_ui/presentation/map_dashboard.dart
+++ lib/system_ui/presentation/map_dashboard.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'map_interactive_search.dart';
-import 'map_page.dart';
+// import 'map_page.dart';
 import 'map_manual_search.dart';
 
 import 'package:buhay/system_ui/controller/map_results_controller.dart';
